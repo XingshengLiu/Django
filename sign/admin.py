11b5ece2ab86cadb34751460a1c6c6d2a2f0953a
liuxingsheng <liuxingsheng@oaserver.dw.gdbbk.com>
2018-07-19 21:01:27 +0800

    1.新建两张数据库表,一张嘉宾表,一张发布会表 2.数据库迁移 3.控制后台展示的列表样式

--- sign/admin.py
+++ sign/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from sign.models import Event, Guest
+
 
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status', 'start_time', 'id']
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realName', 'phone', 'email', 'sign', 'create_time', 'event']
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Guest, GuestAdmin)
